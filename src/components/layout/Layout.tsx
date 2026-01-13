7034980573ecee5186944dd7a1f8ffc869e7c66b
GavinDevelops <gavin.pase.develops@gmail.com>
2026-01-13 07:06:20 +0000

    fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <LazyMotion features={domAnimation} strict>
+    <LazyMotion features={domAnimation}>
       <div className="flex min-h-screen flex-col">
         <Header />
         <main className="flex-1">{children}</main>

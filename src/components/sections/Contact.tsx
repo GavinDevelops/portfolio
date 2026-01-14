afead907df22ecfabb9d0071b3055ed64b0ea9fe
GavinDevelops <gavin.pase.develops@gmail.com>
2026-01-14 04:17:49 +0000

    fix form

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -63,10 +63,12 @@ export function Contact() {
                 name="contact"
                 method="POST"
                 data-netlify="true"
+                data-netlify-honeypot="bot-field"
                 onSubmit={handleSubmit}
                 className="space-y-6"
               >
                 <input type="hidden" name="form-name" value="contact" />
+                <input type="hidden" name="bot-field" />
                 <Input
                   label="Name"
                   name="name"
